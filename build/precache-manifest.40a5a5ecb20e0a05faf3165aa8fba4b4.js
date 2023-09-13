9ae9639a4c88d377fd3a109d49c52b7a0bf6e842
Saurav Pianist <sauravpunworld2@gmail.com>
2023-09-13 23:55:43 +0530

    updated build

---
+++ build/precache-manifest.40a5a5ecb20e0a05faf3165aa8fba4b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d6f0e5ecd2ee0398ae1",
+    "url": "/static/css/main.78674e90.chunk.css"
+  },
+  {
+    "revision": "8d6f0e5ecd2ee0398ae1",
+    "url": "/static/js/main.cfadbbb0.chunk.js"
+  },
+  {
+    "revision": "afbb7d27fc5bbaad97f4",
+    "url": "/static/js/runtime~main.6e03bfec.js"
+  },
+  {
+    "revision": "fe082845bf9bb04be39e",
+    "url": "/static/js/2.1f7305f5.chunk.js"
+  },
+  {
+    "revision": "9392566d3b72faa082df",
+    "url": "/static/js/3.6f8c2c0a.chunk.js"
+  },
+  {
+    "revision": "ee1ef0a696a146e6bcd4c67a776bb7c4",
+    "url": "/index.html"
+  }
+];
